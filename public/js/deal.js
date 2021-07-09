001d9fb722b3a215d571d07ae3a9b02a4079c55c
Dmitry <d1d1d1@inbox.ru>
2021-07-09 15:20:21 +0300

    https routing

--- public/js/deal.js
+++ public/js/deal.js
@@ -318,9 +318,13 @@ $(document).ready(function() {
 	});
 
 	function getDealList(contractorName) {
+		let mix = require('laravel-mix');
+		require('dotenv').config();
+		let url = process.env.APP_URL;
+
 		$.ajax({
 			type: 'GET',
-			url: $('.js-deal-list').attr('action'),
+			url: url + '/deal-list/',
 			data: { contractor: contractorName },
 			dataType: 'json',
 			async: true,
