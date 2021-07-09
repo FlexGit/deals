52f7944ad8a63f6df16fd15f078c2238148ae9e0
Dmitry <d1d1d1@inbox.ru>
2021-07-09 13:51:16 +0300

    https routing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
 		if (app()->environment('remote')) {
-			URL::forceSchema('https');
+			URL::forceScheme('https');
 		}
 		Paginator::useBootstrap();
     }
